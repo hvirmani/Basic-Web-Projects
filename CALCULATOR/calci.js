1530e9d013179022bc896b39278404418d8b1560
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-05-25 00:36:36 +0530

    FINAL CHANGES

--- CALCULATOR/calci.js
+++ CALCULATOR/calci.js
@@ -4,9 +4,20 @@ document.form.textview.value=document.form.textview.value+num;
 
 function equal(){
 var exp=document.form.textview.value;
-if(exp){
-document.form.textview.value=eval(exp);
-}}
+var a=0,b=0;
+    for(var i=0;i<exp.length;i++){
+                 if(exp[i]==='(')
+                     a++;
+                 else if(exp[i]===')')
+                     b++;  
+             }
+             while(a>b){                 document.form.textview.value=document.form.textview.value+')';
+                        b++;
+            }
+           if(exp){
+           document.form.textview.value=eval(exp);
+           }
+}
 function negative(){
 	var exp=document.form.textview.value;
 if(exp){
@@ -20,4 +31,109 @@ document.form.textview.value="";
 function back(){
 var exp = document.form.textview.value;
 document.form.textview.value=exp.substring(0,exp.length-1);
+}
+function display(){
+	var i=1;
+	for(i=0;i<=5;i++){
+		if(document.getElementById("hidden"+i).style.display==="none"){           document.getElementById("textview").style.width="275px"; document.getElementById("hidden"+i).style.display="contents";}
+		else{ document.getElementById("hidden"+i).style.display="none";       document.getElementById("textview").style.width="200px";
+            }
+	}
+}
+function Double(){
+    var exp=document.form.textview.value;
+if(exp){
+document.form.textview.value=eval(exp)*eval(exp);
+}
+}
+function Sine(){
+    var exp=document.form.textview.value;
+    if(exp){    document.form.textview.value=Math.sin(eval(exp));
+    }//if value is given earlier
+    else{
+        document.form.textview.value='Math.sin(';
+    }//if value is entered after
+}
+function Cosine(){
+    var exp=document.form.textview.value;
+    if(exp){    document.form.textview.value=Math.cos(eval(exp));
+    }//if value is given earlier
+    else{
+        document.form.textview.value='Math.cos(';
+    }//if value is entered after
+}
+function Tangent(){
+     var exp=document.form.textview.value;
+    if(exp){    document.form.textview.value=Math.tan(eval(exp));
+    }//if value is given earlier
+    else{
+        document.form.textview.value='Math.tan(';
+    }//if value is entered after
+}
+function Exp(){
+     var exp=document.form.textview.value;
+    if(exp){    document.form.textview.value=Math.exp(eval(exp));
+    }//if value is given earlier
+    else{
+        document.form.textview.value='Math.exp(';
+    }//if value is entered after
+}
+
+function Log(){
+     var exp=document.form.textview.value;
+    if(exp){    document.form.textview.value=Math.log10(eval(exp));
+    }//if value is given earlier
+    else{
+        document.form.textview.value='Math.log10(';
+    }//if value is entered after
+}
+function fact(){
+    var exp=document.form.textview.value;
+    var a=Number(exp);
+    var m=1;
+    for(var i=1;i<=a;i++){
+        m*=i;
+    }
+    document.form.textview.value=m;
+    
+}
+function LN(){
+    var exp=document.form.textview.value;
+    if(exp){    document.form.textview.value=Math.log10(eval(exp));
+    }//if value is given earlier
+    else{
+    document.form.textview.value+='Math.log(';
+    }
+}
+function SQRT(){
+    var exp=document.form.textview.value;
+    if(exp){    document.form.textview.value=Math.sqrt(eval(exp));
+    }//if value is given earlier
+    else{
+    document.form.textview.value+='Math.sqrt(';
+    }
+}
+
+function bracket(){
+    var exp=document.form.textview.value;
+    var a=0,b=0;
+    if(exp.includes('(')){                
+        if(exp[exp.length-1]==='('){
+            document.form.textview.value=document.form.textview.value+'(';
+         }
+      else{
+             for(var i=0;i<exp.length;i++){
+                 if(exp[i]==='(')
+                     a++;
+                 else if(exp[i]===')')
+                     b++;  
+             }
+             
+             if(a>b)                 document.form.textview.value=document.form.textview.value+')';
+             else                 document.form.textview.value=document.form.textview.value+'(';
+      }
+    }
+    else{
+        document.form.textview.value=document.form.textview.value+'(';
+    }
 }
