36b7aed870c0d5495c9d794aa87b7521dfee653c
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-05-29 23:39:49 +0530

    Some changes

--- TO DO LIST/todo.js
+++ TO DO LIST/todo.js
@@ -1,3 +1,28 @@
+var arr = document.getElementsByTagName("li");
+        for (var i = 0; i < arr.length; i++) {
+            var span = document.createElement('span');
+            var txt = document.createTextNode('X');
+            span.className = "close";
+            span.appendChild(txt);
+            arr[i].appendChild(span);
+        }
+        //alert(arr.length);
+
+        var close = document.getElementsByClassName("close");
+        var i;
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+        var list = document.querySelector('ul');
+        list.addEventListener('click', function(ev) {
+            if (ev.target.tagName === 'LI') {
+                ev.target.classList.toggle('checked');
+            }
+        }, false);
+
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
