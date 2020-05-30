763d6ebbc37d4120a6566c8647698cfb12cd9724
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-05-30 23:42:22 +0530

    On the way

--- TO DO LIST/todo.js
+++ TO DO LIST/todo.js
@@ -14,10 +14,8 @@ for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
         var div = this.parentElement;
         var complete = document.createElement('done');
-        var done_value = div.firstChild.nodeValue;
-        var txt = document.createTextNode(done_value);
-        complete.appendChild(txt);
-        document.getElementById('completed').appendChild(complete);
+        var txt = div.firstChild;
+        complete.appendChild(txt); document.getElementById('completed').appendChild(complete);
         div.style.display = "none";
     }
 }
