08941f0870a7d551db74bc049319c166abf70d2d
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-05-30 14:24:44 +0530

    In Progress

--- TO DO LIST/todo.js
+++ TO DO LIST/todo.js
@@ -1,50 +1,75 @@
-var arr = document.getElementsByTagName("li");
-        for (var i = 0; i < arr.length; i++) {
-            var span = document.createElement('span');
-            var txt = document.createTextNode('X');
-            span.className = "close";
-            span.appendChild(txt);
-            arr[i].appendChild(span);
-        }
-        //alert(arr.length);
-
-        var close = document.getElementsByClassName("close");
-        var i;
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function() {
-                var div = this.parentElement;
-                div.style.display = "none";
-            }
-        }
-        var list = document.querySelector('ul');
-        list.addEventListener('click', function(ev) {
-            if (ev.target.tagName === 'LI') {
-                ev.target.classList.toggle('checked');
-            }
-        }, false);
-
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+//Put a cross button on evry item in a list
+var arr = document.getElementsByClassName("active");
+for (var i = 0; i < arr.length; i++) {
+    var span = document.createElement('span');
+    var txt = document.createTextNode('X');
+    span.className = "close";
+    span.appendChild(txt);
+    arr[i].appendChild(span);
+}
+//Function of close button
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        var complete = document.createElement('done');
+        var done_value = div.firstChild.nodeValue;
+        var txt = document.createTextNode(done_value);
+        complete.appendChild(txt);
+        document.getElementById('completed').appendChild(complete);
+        div.style.display = "none";
     }
-  }
+}
+
+function appear() {
+    if (document.getElementById('ip').style.display === "none") {
+        document.getElementById('ip').style.display = "block";
+    } else {
+        newElement();
+    }
+}
+function see(){
+    if(document.getElementById('active').style.display==="none"){
+        document.getElementById('active').style.display="block";
+        document.getElementById('completed').style.display="none"
+    }
+    else{
+        {
+        document.getElementById('active').style.display="none";
+        document.getElementById('completed').style.display="block"
+    }
+    }
+}
+function newElement() {
+    var element = document.createElement('li');
+    var value = document.getElementById('ip').value;
+    var txt = document.createTextNode(value);
+    element.appendChild(txt);
+    if (value === '') {
+        alert('You must write something');
+    } else {
+        var x = document.getElementById('active');
+        x.appendChild(element);
+    }
+    document.getElementById('ip').value = "";
+
+    var span = document.createElement('span');
+    var txt = document.createTextNode('X');
+    span.className = "close";
+    span.appendChild(txt);
+    element.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            var div = this.parentElement;
+            var complete = document.createElement('done');
+            var done_value = div.firstChild.nodeValue;
+            var txt = document.createTextNode(done_value);
+            complete.appendChild(txt);
+            document.getElementById('completed').appendChild(complete);
+            div.style.display = "none";
+            div.style.display = "none";
+        }
+    }
 }
