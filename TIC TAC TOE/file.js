460243d90816c9a249f1145acb5f1cbb85d84530
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-06-14 23:38:39 +0530

    Final Commit

--- TIC TAC TOE/file.js
+++ TIC TAC TOE/file.js
@@ -1,31 +1,44 @@
-var ct = 0,
-    value, player;
+var ct = 0,value, player;
 if (!(ct % 2)) {
     value = 'X';
     player = 1;
-} else {
+} 
+else {
     value = 'O';
     player = 2;
 }
 //alert(value);
 function win() {
-    if ((($('#1').text() === value) && ($('#2').text() === value) && ($('#3').text() === value)) || (($('#4').text() === value) && ($('#5').text() === value) && ($('#6').text() === value)) || (($('#7').text() === value) && ($('#8').text() === value) && ($('#9').text() === value))) //horizontal win
+    if ((($('#a').text() === value) && ($('#b').text() === value) && ($('#c').text() === value)) || (($('#d').text() === value) && ($('#e').text() === value) && ($('#f').text() === value)) || (($('#g').text() === value) && ($('#h').text() === value) && ($('#i').text() === value))) //horizontal win
     {
-        alert(value);
+        $(".result").show(5000);
+        $("#res_ult").text("Player "+player+" Wins");
+        $("#res").text("Play Again");
+        $("button").attr("disabled","true"); document.getElementById("res").disabled = false;        
         return;
     }
-    if ((($('#1').text() === value) && ($('#4').text() === value) && ($('#7').text() === value)) || (($('#2').text() === value) && ($('#5').text() === value) && ($('#8').text() === value)) || (($('#3').text() === value) && ($('#6').text() === value) && ($('#9').text() === value))) //vertical win
+    if ((($('#a').text() === value) && ($('#d').text() === value) && ($('#g').text() === value)) || (($('#b').text() === value) && ($('#e').text() === value) && ($('#h').text() === value)) || (($('#c').text() === value) && ($('#f').text() === value) && ($('#i').text() === value))) //vertical win
     {
-        alert(value);
+        $(".result").show(3000);
+        $("#res_ult").text("Player "+player+" Wins");
+        $("#res").text("Play Again");
+        $("button").attr("disabled","true"); document.getElementById("res").disabled = false;        
         return;
     }
-    if ((($('#1').text() === value) && ($('#5').text() === value) && ($('#9').text() === value)) || (($('#3').text() === value) && ($('#5').text() === value) && ($('#7').text() === value))) //diagonal win
+    if ((($('#a').text() === value) && ($('#e').text() === value) && ($('#i').text() === value)) || (($('#c').text() === value) && ($('#e').text() === value) && ($('#g').text() === value))) //diagonal win
     {
-        alert(value);
+        $(".result").show(3000);
+        $("#res_ult").text("Player "+player+" Wins");
+        $("#res").text("Play Again");
+        $("button").attr("disabled","true"); document.getElementById("res").disabled = false;        
         return;
     }
     if (ct === 8) {
-        alert("Draw");
+        $(".result").show(3000);
+        $("#res_ult").text("Match Draw");
+        $("#res").text("Play Again");
+        $("button").attr("disabled","true"); document.getElementById("res").disabled = false;        
+        return;
     }
 }
 $(document).ready(function () {
@@ -37,19 +50,39 @@ $(document).ready(function () {
             value = 'O';
             player = 2;
         }
-        var x = $(this).text();
-        if (x === "X" || x === "O") {
+        var x = $(this).attr("class");
+        if (x === "done") {
             alert("Position Already Occupied");
             return;
-        } else
+        } else {
             $(this).text(value);
+            $(this).attr("class", "done");
+        }
         win();
         ct++;
     });
     $("button").mouseover(function () {
-        $(this).css("background", "red");
+        if (!(ct % 2)) {
+            value = 'X';
+            player = 1;
+        } else {
+            value = 'O';
+            player = 2;
+        }
+        if ((!($(this).attr("class") === "done"))&&(!($(this).attr("id") === "res")))
+            $(this).text(value);
     });
     $("button").mouseout(function () {
-        $(this).css("background", "gray");
+        if ((!($(this).attr("class") === "done"))&&(!($(this).attr("id") === "res")))
+        $(this).text('');
+    });
+    $("#res").click(function () {
+    $(".done").text("");
+    $(".done").attr("class", "");
+    //$("button").attr("disabled","false");
+    $('button').prop('disabled', false);
+    $(".result").hide(2000);
+        ct=0;
+    $(this).text("");
     });
 })
