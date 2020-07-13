b059db4c74ec57161eb532261d6d4d3ed252914a
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-07-13 23:33:13 +0530

    Initial Commit

--- BUDGET/file.js
+++ BUDGET/file.js
@@ -1,8 +1,27 @@
 var income;
-var expense;
-var balance;
+var expense_msg,expense_amt;
+var wish_msg,wish_amt;
+var balance=0;
 $(document).ready(function(){
   $("#inc").click(function(){
 	  income=$("#income").val();
+	  balance+=income;
+	  alert(income);
+	  //alert(income);
+  });
+  $("#expense").click(function(){
+	expense_amt=$("#expense_amount").val();
+	expense_msg=$("#expense_text").val();
+	  alert(income);
+    if(expense_amt<balance){
+				
+
+		$("#expense_list").append(`<tr><td>${expense_amt}</td><td>${expense_msg}</td></tr>`);
+		
+	}	
+	});
+  $("#wish").click(function(){
+	  wish_msg=$("#wish_text").val();
+	  wish_amt=$("#wish_amount").val();
   });
 });
