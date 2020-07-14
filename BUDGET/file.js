14d8d7b51234fa470c2a51164bf4fb991e671a2d
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-07-14 23:10:46 +0530

    Initial Commit

--- BUDGET/file.js
+++ BUDGET/file.js
@@ -1,27 +1,29 @@
-var income;
-var expense_msg,expense_amt;
-var wish_msg,wish_amt;
-var balance=0;
+//var income;
+//var expense_msg,expense_amt;
+//var wish_msg,wish_amt;
+//var balance=0;
 $(document).ready(function(){
   $("#inc").click(function(){
-	  income=$("#income").val();
-	  balance+=income;
-	  alert(income);
-	  //alert(income);
-  });
+	  var income=$("#income").val();
+	  var balance=$("#balance_txt").text();
+	  balance=balance+income;
+	  alert(balance);
+	  $("#income_txt").text(income);
+	  $("#balance_txt").text(balance);
+  });/*
   $("#expense").click(function(){
-	expense_amt=$("#expense_amount").val();
-	expense_msg=$("#expense_text").val();
-	  alert(income);
+	var expense_amt=$("#expense_amount").val();
+	var expense_msg=$("#expense_text").val();
+	var balance=
     if(expense_amt<balance){
 				
 
 		$("#expense_list").append(`<tr><td>${expense_amt}</td><td>${expense_msg}</td></tr>`);
 		
 	}	
-	});
-  $("#wish").click(function(){
+	});*/
+  /*$("#wish").click(function(){
 	  wish_msg=$("#wish_text").val();
 	  wish_amt=$("#wish_amount").val();
-  });
+  });*/
 });
